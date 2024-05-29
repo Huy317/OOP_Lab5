146e8635b7d8cb8227aa11bd6354f2bf7016f258
Huy317 <167590855+Huy317@users.noreply.github.com>
2024-05-29 13:49:56 +0700

    fixed infinite try catch bug

--- UniversityManagement/AskInput.java
+++ UniversityManagement/AskInput.java
@@ -5,43 +5,51 @@ import java.util.Scanner;
 
 public class AskInput {
     Scanner sc = new Scanner(System.in);
-    public AskInput(){
+
+    public AskInput() {
     }
-    public String askString(String message){
+
+    public String askString(String message) {
         String a;
         while (true) {
             try {
                 System.out.print(message);
                 a = sc.nextLine();
                 break;
-            }catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("something went wrong");
+                sc.nextLine();
             }
         }
         return a;
     }
-    public int askInt(String message){
+
+    public int askInt(String message) {
         int a;
         while (true) {
             try {
                 System.out.print(message);
                 a = sc.nextInt();
+
                 break;
-            }catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("Please enter Integer only");
+                sc.nextLine();
             }
         }
         return a;
     }
-    public double askDouble(String message){
+
+    public double askDouble(String message) {
         double a;
         while (true) {
             try {
                 System.out.print(message);
                 a = sc.nextDouble();
                 break;
-            }catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("Please enter double only");
+                sc.nextLine();
             }
         }
         return a;
